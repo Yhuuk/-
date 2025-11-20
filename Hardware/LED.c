bd6ecc33fc57b1b357248074b829b5d67c817e21
欧北离 <3359130875@qq.com>
2025-11-20 17:57:18 +0800

    这是主逻辑函数优化后的代码，只有三个LED模式

--- Hardware/LED.c
+++ Hardware/LED.c
@@ -4,6 +4,18 @@
 uint8_t change_mode = 0;
 bool stop_flag = 0;
 
+/*---------------- 参数&类型定义 ----------------*/
+#define FIRST_GEAR_MIN   950
+#define FIRST_GEAR_MAX   1300
+
+typedef enum
+{
+    LED_MODE_FIRST = 0,
+    LED_MODE_SECOND,
+    LED_MODE_THIRD,
+    LED_MODE_COUNT       // 模式数量，用来做取模
+} LED_Mode;
+
 /**
   * 函    数：LED初始化
   * 参    数：无
@@ -122,59 +134,79 @@ void LED_ThirdMode(void)
 }
 
 
+/*---------------- 内部小工具函数 ----------------*/
+
+static inline bool is_first_gear(uint16_t pwm)
+{
+    return (pwm >= FIRST_GEAR_MIN) && (pwm < FIRST_GEAR_MAX);
+}
+
+
+static void LED_ApplyMode(uint8_t mode)
+{
+    switch (mode)
+    {
+        case LED_MODE_FIRST:
+            LED_FirstMode();
+            break;
+        case LED_MODE_SECOND:
+            LED_SecondMode();
+            break;
+        case LED_MODE_THIRD:
+            LED_ThirdMode();
+            break;
+        default:
+            // 理论上不会走到这里，当保护用
+            LED_initial_mode();
+            break;
+    }
+}
+
+/*---------------- 主逻辑函数 ----------------*/
 
 void LED_Switch(void)
 {
-	/************************************************************************/
-	if(TIM_GetCapture2(TIM1) >= 950 && TIM_GetCapture2(TIM1) <1300) // 拨杆一档
-	{
-		stop_flag = 1;
+    uint16_t ch_val = TIM_GetCapture2(TIM1);
 
-	}else
-	{
-		stop_flag = 0;
-		Delay_ms(5);
-	}
+    /********** 1. 根据拨杆位置设置 stop_flag **********/
+    if (is_first_gear(ch_val))
+    {
+        stop_flag = 1;
+    }
+    else
+    {
+        stop_flag = 0;
+        Delay_ms(5);
+    }
 
-	/************************************************************************/
-	if (first_gear_flag == 1)
-	{
-		first_gear_flag = 0;
-		change_mode = 0;
-	}
-	
-	
-	/************************************************************************/
-	if(state_flag == 1)
-	{
+    /********** 2. 一档切换成二档触发：恢复到初始模式 **********/
+    if (first_gear_flag)
+    {
+        first_gear_flag = 0;
+        change_mode = 0;
+    }
+
+    /********** 3. 切换模式：state_flag 上升沿触发 **********/
+    if (state_flag)
+    {
         state_flag = 0;
-		change_mode++;
-		if(change_mode >2)
-		{
-			change_mode = 0;
-		}
-	}
 
-	/************************************************************************/
-	if(stop_flag ==1)
-	{
-		LED_initial_mode();
-	}
-	else
-	{
-		if(change_mode == 0)
-		{
-			LED_FirstMode();
-		}
-		else if(change_mode == 1)
-		{
-			LED_SecondMode();
-		}
-		else if(change_mode == 2)
-		{
-			LED_ThirdMode();
-		}
-		
-	}
-		
+        change_mode++;
+        if (change_mode >= LED_MODE_COUNT)
+        {
+            change_mode = 0;
+        }
+    }
+
+    /********** 4. 根据 stop_flag/模式，实际执行灯效 **********/
+    if (stop_flag)
+    {
+        // 拨杆在“一档” -> 进入初始模式（全部熄灭 或 某种安全模式）
+        LED_initial_mode();
+    }
+    else
+    {
+        // 按当前模式执行灯效
+        LED_ApplyMode(change_mode);
+    }
 }
